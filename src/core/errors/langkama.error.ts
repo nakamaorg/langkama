b725d0f10dc3bcf33ab9e59bcc13f53f02c02026
eoussama <oussama.essamadi@gmail.com>
2024-03-07 23:53:12 +0100

    error managemet

---
+++ src/core/errors/langkama.error.ts
@@ -0,0 +1,16 @@
+export class LangKamaError extends Error {
+
+  private row: number;
+  private col: number;
+
+  constructor(row: number, col: number, message: string) {
+    super(message);
+
+    this.row = row;
+    this.col = col;
+  }
+
+  public toString(): string {
+    return `[${this.row}:${this.col}] ${this.message}.`;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,31 @@
-// import { Parser } from './frontend/parser';
-// import { evaluate } from './runtime/interpreter';
-// import { Environment } from './runtime/environment';
+import { Lexer } from './frontend/lexer';
+import { Parser } from './frontend/parser';
+import { evaluate } from './runtime/interpreter';
+import { Environment } from './runtime/environment';
 
-import { tokenize } from "./frontend/lexer";
-
-
-
-// const code = `
-//   const birthyear = 1998.
-//   let curryear = 2024.
-//   let age = curryear - birthyear.
-
-//   age
-// `;
-
-// try {
-//   const parser = new Parser();
-//   const env = new Environment();
-
-//   const program = parser.parse(code);
-//   const result = evaluate(program, env);
-
-//   console.log(result);
-// } catch (err) {
-//   console.error(`Error: ${err}`);
-// }
 
 
 const code = `
-a sa7 hear me out x is 14.
-hear me out y is 12.
-hear me out a is L.
-hear me out b is W.
-hear me out c is bruh.
+  a sa7 hear me out birthyear is 1998.
+  hear me out curryear is 2024.
+  hear me out age is curryear - birthyear.
+
+  age
 `;
 
 try {
-  const tokens = tokenize(code);
-  console.log(tokens);
+  const lexer = new Lexer();
+  const parser = new Parser();
+  const env = new Environment();
+
+  console.log('Compiling LangKama script...');
+
+  const tokens = lexer.tokenize(code);
+  const program = parser.parse(tokens);
+  const result = evaluate(program, env);
+
+  console.log('LangKama script compiled!\n');
+  console.log({ result });
 } catch (err) {
-  console.error(`Error: ${err}`);
+  console.error(`LangKama Error:\n  ${err}`);
 }
