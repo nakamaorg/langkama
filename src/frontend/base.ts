c348f5e2001f1e05a1dc2f2c845c4593d8c1b1ba
EOussama <oussama.essamadi@gmail.com>
2024-03-12 10:51:47 +0000

    base frontend constructor

--- src/frontend/base.ts
+++ src/frontend/base.ts
@@ -2,6 +2,28 @@
  * @description
  * Helps with token/character traversal
  */
-export class Base {
+export class Base<T> {
+  
+  /**
+   * @description
+   * Content to traverse 
+  */
+  private content!: T;
 
+  /**
+   * @description
+   * The index where the traversal is currently on 
+   */
+  private index!: number;
+
+  /**
+   * @description
+   * Instntiates a base frontend instance
+   *
+   * @param content The content to reset to
+   */
+  public init(content: T): void {
+    this.index = 0;
+    this.content = content;
+  }
 }
