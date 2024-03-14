f8ce215d822e9e0a796631381821b73dbdfd5124
eoussama <oussama.essamadi@gmail.com>
2024-03-14 18:33:20 +0000

    string unit tests

--- test/string.test.js
+++ test/string.test.js
@@ -1,39 +1,46 @@
 const Errno = require('../dist/langkama.umd.cjs').Errno;
 const LangKama = require('../dist/langkama.umd.cjs').LangKama;
+const LangKamaEvent = require('../dist/langkama.umd.cjs').LangKamaEvent;
 
 
 
 describe('Strings', () => {
+  let compiler;
+
+  beforeEach(() => {
+    compiler = new LangKama();
+  });
+
   test('String value', async () => {
     const code = `"John"`;
-    const result = await LangKama.interpret(code);
-
-    expect(result.value).toBe('John');
+    compiler.on(LangKamaEvent.Success, result => expect(result.value).toBe('John')).interpret(code);
   });
 
   test('String with spaces', async () => {
     const code = `"John Doe"`;
-    const result = await LangKama.interpret(code);
-
-    expect(result.value).toBe('John Doe');
+    compiler.on(LangKamaEvent.Success, result => expect(result.value).toBe('John Doe')).interpret(code);
   });
 
   test('String concatenation', async () => {
     const code = `"John" + " " + "Doe"`;
-    const result = await LangKama.interpret(code);
-
-    expect(result.value).toBe('John Doe');
+    compiler.on(LangKamaEvent.Success, result => expect(result.value).toBe('John Doe')).interpret(code);
   });
 });
 
 describe('Strings errors', () => {
+  let compiler;
+
+  beforeEach(() => {
+    compiler = new LangKama();
+  });
+
   test('Incomplete operation', () => {
     const code = `"John" +`;
-    LangKama.interpret(code).catch(err => expect(err.errno).toBe(Errno.IncompleteExpressionError));
+    compiler.on(LangKamaEvent.Error, error => expect(error.errno).toBe(Errno.IncompleteExpressionError)).interpret(code);
   });
-  
+
   test('String concatenation with non string type', () => {
     const code = `"John" + 1`;
-    LangKama.interpret(code).catch(err => expect(err).toBe('Can\'t perform binary operations on different types'));
+    compiler.on(LangKamaEvent.Error, error => expect(error).toBe('Can\'t perform binary operations on different types')).interpret(code);
   });
 });
