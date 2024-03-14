d3d56253a7810c62a8827c9bdcb8fdd0a2485b1c
eoussama <oussama.essamadi@gmail.com>
2024-03-14 18:33:27 +0000

    constants unit tests

--- test/constants.test.js
+++ test/constants.test.js
@@ -1,14 +1,19 @@
 const Errno = require('../dist/langkama.umd.cjs').Errno;
 const LangKama = require('../dist/langkama.umd.cjs').LangKama;
+const LangKamaEvent = require('../dist/langkama.umd.cjs').LangKamaEvent;
 
 
 
 describe('Constants', () => {
+  let compiler;
+
+  beforeEach(() => {
+    compiler = new LangKama();
+  });
+
   test('Constant declaration with value', async () => {
     const code = `a sa7 hear me out zero is 0.`;
-    const result = await LangKama.interpret(code);
-
-    expect(result.value).toBe(0);
+    compiler.on(LangKamaEvent.Success, result => expect(result.value).toBe(0)).interpret(code);
   });
 
   test('Multiple constant declarations', async () => {
@@ -18,15 +23,20 @@ describe('Constants', () => {
       a sa7 hear me out fullName is firstName + " " + lastName.
     `;
 
-    const result = await LangKama.interpret(code);
-    expect(result.value).toBe('John Doe');
+    compiler.on(LangKamaEvent.Success, result => expect(result.value).toBe('John Doe')).interpret(code);
   });
 });
 
 describe('Constants errors', () => {
+  let compiler;
+
+  beforeEach(() => {
+    compiler = new LangKama();
+  });
+
   test('Constant declaration without a value', () => {
     const code = `a sa7 hear me out zero.`;
-    LangKama.interpret(code).catch(err => expect(err.errno).toBe(Errno.UninitializedConstantError));
+    compiler.on(LangKamaEvent.Error, error => expect(error.errno).toBe(Errno.UninitializedConstantError)).interpret(code);
   });
 
   test('Constant duplication', () => {
@@ -34,19 +44,19 @@ describe('Constants errors', () => {
       a sa7 hear me out zero is 0.
       a sa7 hear me out zero is 0.
     `;
-    LangKama.interpret(code).catch(err => expect(err).toBe('Cannot declare variable zero as it\'s already defined'));
+    compiler.on(LangKamaEvent.Error, error => expect(error).toBe('Cannot declare variable zero as it\'s already defined')).interpret(code);
   });
 
   test('Constant reassignment', () => {
     const code = `
-      a sa7 hear me out one is 1.
-      one is 2.
+    a sa7 hear me out one is 1.
+    one is 2.
     `;
-    LangKama.interpret(code).catch(err => expect(err).toBe('Constant can\'t be reassigned'));
+    compiler.on(LangKamaEvent.Error, error => expect(error).toBe('Constant can\'t be reassigned')).interpret(code);
   });
 
   test('Undeclared constant', () => {
     const code = `myVar`;
-    LangKama.interpret(code).catch(err => expect(err).toBe('Cannot resolve variable "myVar" as it does not exist'));
+    compiler.on(LangKamaEvent.Error, error => expect(error).toBe('Cannot resolve variable "myVar" as it does not exist')).interpret(code);
   });
 });
