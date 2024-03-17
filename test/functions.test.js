001042588dff894c70c0d83c01cb025dc5f553c0
eoussama <oussama.essamadi@gmail.com>
2024-03-17 23:46:13 +0000

    function unit tests

---
+++ test/functions.test.js
@@ -0,0 +1,29 @@
+const Errno = require('../dist/langkama.umd.cjs').Errno;
+const LangKama = require('../dist/langkama.umd.cjs').LangKama;
+const LangKamaEvent = require('../dist/langkama.umd.cjs').LangKamaEvent;
+
+
+
+describe('Functions', () => {
+  let compiler;
+
+  beforeEach(() => {
+    compiler = new LangKama();
+  });
+
+  test('Function definition', done => {
+    const code = `
+      let me cook add(a, b) {
+        reda a + b;
+      }
+
+      add(1, 2);
+    `;
+
+    compiler.on(LangKamaEvent.Success, result => {
+        expect(result.value).toBe(3);
+        done();
+      })
+      .interpret(code);
+  });
+});
